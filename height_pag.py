0def7b441f5d9317978e3525f936c87cf340e981
onigari <50879694+proteekrosul@users.noreply.github.com>
2020-11-03 23:34:26 +0600

    Update height_pag.py

--- height_pag.py
+++ height_pag.py
@@ -1,5 +1,5 @@
 # this method is much easier and quicker, but since pyautogui is not a built-in module, the classic tkinter approach was also attempted.
 
-from pyautogui import prompt, alert
+from pyautogui import *
 h = prompt(text="Enter your height: ", title="Height calculator", default='')
 alert(text=f"Your height is {h}!", title="Height calculator", button="OK")
